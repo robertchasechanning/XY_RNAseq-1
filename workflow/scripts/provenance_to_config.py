63ed1d2a4793f391bbe1fa2fc853d9a92d088898
robertchasechanning <robert.chase@channing.harvard.edu>
2022-07-21 15:05:06 -0400

    add function for parsing the file list

--- workflow/scripts/provenance_to_config.py
+++ workflow/scripts/provenance_to_config.py
@@ -1,25 +1,42 @@
 #The purpose of this script is to read in a transferqc provenance file and creata json config file for the xyalign pipeline
 
 import argparse
+import yaml
+from yaml.loaders import FullLoader
+import json
+import pandas as pd
+
+def create_sampleconfig_from_provenance_config(provenance_config):
+    """Creates a sample configuration object from a provenance configuration object."""
+    merged_filenames = provenance_config["merged_filename"]
+    return sample_config
 
 def read_provenance_file(provenance_filename):
     """Reads a transfer_qc provenance yaml file and return a configuration dictionary."""
-    pass
+    provenance_file = open(provenance_filename, "r")
+    provenance_filestring = provenance_file.read()
+    provenance_config = yaml.read_yaml(provenance_filestring, loader="FullLoader")
+    sample_config = create_sampleconfig_from_provenance_config(provenance_config)
+    provenance_config["sample_config"] = sample_config
     return provenance_config
 
 def read_gender_file(gender_filename):
     """Reads a gender file and returns a sample configuration dictionary."""
-    pass
+    gender_df = pd.read_csv(gender_filename)
+    gender_config = dict(gender_df)
     return gender_config
 
 def generate_combined_dictionary(provenance_config, gender_config):
     """Generates a configuration object."""
-    pass
+    combined_config = provenance_config
+    combined_config["sample_config"].update(gender_config)
     return combined_config
 
 def create_json_config(combined_config, json_filename):
     """Generates a json file from a combined config and saves it to the filesystem."""
-    pass
+    json_string = json.dump(combined_config)
+    json_filehandle = open(json_filename, "w")
+    json_filehandle.write(json_string)
 
 def provenance_to_config(provenance_filename, gender_filename, json_filename):
     """This program generates a config."""
@@ -28,18 +45,31 @@ def provenance_to_config(provenance_filename, gender_filename, json_filename):
     combined_config = generate_combined_dictionary(provenance_config, gender_config)
     create_json_config(combined_config, json_filename)
 
-def test_provenance_to_config(provenance_filename, gender_filename, json_filename)
-    provenance_filename = args #FIXME
-    gender_filename = args #FIXME
-    json_filename = args #FIXME
-    provenance_to_config(provenance_filename, gender_filename, json_filename)
-    
-def main():
-    """parses arguments to get provenance_filename, gender_filename, json_filename and runs provenance_to_config."""
-    provenance_filename = args #FIXME
-    gender_filename = args #FIXME
-    json_filename = args #FIXME
+def test_args():
+    """Returns test arguments."""
+    provenance_filename = "/proj/regeps/regep00/studies/COPDGene/data/rna/mrna/blood_shortread/data/raw/ppm_transfer_batches/2019-10-31_for_rpc15/COPD/191010_7001411_0900_ACDMK1ANXX/project_COPDGene_rna_mrna_phase2_191010_7001411_0900_ACDMK1ANXX.yaml"
+    gender_filename = "resources/COPDGene_P1P2P3_SM_NS_Long_Oct21.txt"
+    json_filename = "Processing/channing.config.automatedtest.json"
+    return (provenance_filename, gender_filename, json_filename)
+
+def test_provenance_to_config(provenance_filename, gender_filename, json_filename):
+    """Test Function"""
+    provenance_filename,gender_filename,json_filename = test_args()
     provenance_to_config(provenance_filename, gender_filename, json_filename)
 
-if __name__=="__main__":
-    main()
+def gather_args():
+    """Gathers args from the command line."""
+    provenance_filename = args #FIXME
+    gender_filename = args #FIXME
+    json_filename = args #FIXME
+    return (provenance_filename, gender_filename, json_filename)
+
+def main():
+    """parses arguments to get provenance_filename, gender_filename, json_filename and runs provenance_to_config."""
+    provenance_filename,gender_filename,json_filename = gather_args()
+    provenance_to_config(provenance_filename, gender_filename, json_filename)
+
+if __name__ == "__main__":
+    """Runs the testing function. Eventually Will run from command line arguments."""
+    test_provenance_to_config()
+    #main()
